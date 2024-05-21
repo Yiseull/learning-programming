1abddb3e3785111dc62b795227e0934c45b57729
Yiseul Park <omjl5123@catholic.ac.kr>
2024-05-21 22:38:32 +0900

    [kafka] refactor: kafka 설정 정보 yml 파일로 추출

--- kafka-consumer/src/main/java/com/kafka/consumer/KafkaConsumerConfig.java
+++ kafka-consumer/src/main/java/com/kafka/consumer/KafkaConsumerConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -13,11 +14,18 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
 @Configuration
 public class KafkaConsumerConfig {
+
+	@Value("${spring.kafka.bootstrap-servers}")
+	private String bootstrapAddress;
+
+	@Value("${spring.kafka.consumer.group-id}")
+	private String groupId;
+
 	@Bean
 	public ConsumerFactory<String, String> consumerFactory() {
 		Map<String, Object> config = new HashMap<>();
-		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		config.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer_group");
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+		config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		return new DefaultKafkaConsumerFactory<>(config);
--- kafka-producer/src/main/java/com/kafka/producer/KafkaTopicConfig.java
+++ kafka-producer/src/main/java/com/kafka/producer/KafkaTopicConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -15,10 +16,14 @@ import org.springframework.kafka.core.KafkaAdmin;
  */
 @Configuration
 public class KafkaTopicConfig {
+
+	@Value("${spring.kafka.bootstrap-servers}")
+	private String bootstrapAddress;
+
 	@Bean
 	public KafkaAdmin kafkaAdmin() {
 		Map<String, Object> config = new HashMap<>();
-		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
 		return new KafkaAdmin(config);
 	}
 
